957591f12b7cf7ea8e0ff76d6bd3b499bdf5d358
heyj97 <nb3065@gmail.com>
2026-06-05 18:53:31 +0900

    Refactor: Cron을 위한 railway heartbeat시스템 도입

--- src/jobs/attendanceCron.js
+++ src/jobs/attendanceCron.js
@@ -3,7 +3,7 @@ const { sendAttendanceStatus } = require("../services/attendanceService");
 const { registerJob } = require("../system/cronManager");
 
 function startAttendanceJob(client) {
-    const schedules = [15, 18, 21];
+    const schedules = [15, 18, 19, 21];
 
     schedules.forEach(hour => {
         const job = cron.schedule(
